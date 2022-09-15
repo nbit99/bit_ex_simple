4abc212cc38dadfe2cf8db0cdb8fc4adf081c0f9
yangpro <y62069061@gmail.com>
2022-09-15 13:48:27 +0800

    remove no use constant

--- src/main/java/io/ex/notice/APIConstants.java
+++ src/main/java/io/ex/notice/APIConstants.java
@@ -1,6 +1,4 @@
 package io.ex.notice;
-import java.util.Arrays;
-import java.util.List;
 
 public class APIConstants {
 
@@ -27,52 +25,7 @@ public class APIConstants {
 
     public static final String EMPTY = "";
 
-    public static final List<String> toStringTypeArray = Arrays.asList(
-            "class java.lang.Long",
-            "class java.lang.Integer",
-            "long",
-            "int");
-    public static final List<String> toStringTypeDoubleArray = Arrays.asList(
-            "class java.lang.Double",
-            "double");
 
-    public static final List<Integer> resultStatusArray = Arrays.asList(
-            400,401,429,500);
-
-    public static final String BOOLEAN = "boolean";
-    public static final String IS = "is";
-    public static final String get = "get";
-    public static final char a = 'a';
-    public static final char z = 'z';
-    public static final String ZERO_STRING = "0";
-    public static final String DOUBLE_ZERO_STRING = "0.00";
-
-    public static final String DOT1 = ".";
-    public static final String DOT2 = "\\.";
-    public static final String E = "E";
-    public static final String e = "e";
-    public static final int DEFAULT_SCALE = 2;
-    /**
-     * 8900000000.000000000
-     */
-    public static final String DOUBLE_END1 = "0+?$";
-    /**
-     * 8900000000.
-     */
-    public static final String DOUBLE_END2 = "[.]$";
-
-    /**
-     * default cursor id
-     */
-    public static final int ONE = 1;
-    /**
-     * max limit: 100
-     */
-    public static final int HUNDRED = 100;
-
-    public static final String HLINE = "-";
-
-    public static final String SLASH = "/";
 
     public static final String timestamp_key = "timestamp";
 
